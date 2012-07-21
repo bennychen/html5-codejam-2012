5cad5c2963fa35f5da0ed46e5fb9dd97126f7588
Yu Hong <hello.hyu@gmail.com>
2012-07-21 16:53:47 +0800

    修改错误

--- js/Level.js
+++ js/Level.js
@@ -1,42 +1,42 @@
 var IngredientTypes = ["onion", "tomato", "egg", "cheese", "meat", "lettuce"];
 
-var IngredientPrice = {
-	{ onion:0.69 },
+var IngredientPrice = [
+{ onion:0.69 },
 	{ tomato: 0.57 },
 	{ egg:0.57 },
 	{ meat:0.86 },
 	{ lettuce:0.57 }
-}
+]
 
-var easyOrders = {};
-var normalOrders = {};
-var hardOrders = {};
+var easyOrders = [];
+var normalOrders = [];
+var hardOrders = [];
 
 var level = null;
 
 
 easyOrders.push({
 		 name:'Prizeburger',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num: 1 },
 			 {type:'tomato', num: 1 },
 			 {type:'cheese', num: 3 },
 			 {type:'meat', num: 1 },
-		 }
+		 ]
  });
 easyOrders.push({
 		 name:'BLOOP',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num:4},
 			 {type:'tomato', num: 2 },
 			 {type:'cheese', num: 2 },
 			 {type:'meat', num: 4 },
-		 }
+		 ]
  });
 	 	
 normalOrders.push({
 		 name:'Maxburger',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num:9},
 			 {type:'tomato', num: 9 },
 			 {type:'cheese', num: 9 },
@@ -44,71 +44,71 @@ normalOrders.push({
 			 {type:'egg', num: 9 },
 			 {type:'lettuce', num: 9 },
 			 
-		 }
+		 ]
  });
 normalOrders.push({
 		 name:'Idontknowwhat',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num:8},
 			 {type:'tomato', num: 4 },
 			 {type:'cheese', num: 4 },
 			 {type:'meat', num: 4 },
-			 {type:'egg', num: 4 }		 }
+			 {type:'egg', num: 4 }		 ]
 	 });
 normalOrders.push({
 		 name:'Idontknowwhat',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num:8},
 			 {type:'tomato', num: 4 },
 			 {type:'cheese', num: 4 },
 			 {type:'meat', num: 4 },
-			 {type:'egg', num: 4 }		 }
+			 {type:'egg', num: 4 }		 ]
 	 });
 normalOrders.push({
 		 name:'PLOT',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num:5},
 			 {type:'tomato', num: 5 },
 			 {type:'lettuce', num: 5 },
 			 {type:'meat', num: 15 },
-			 {type:'egg', num: 5 }		 }
+			 {type:'egg', num: 5 }		 ]
 	 });
 normalOrders.push({
 		 name:'Tomaintno',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num:6},
 			 {type:'lettuce', num: 6 },
 			 {type:'meat', num: 12 },
-			 {type:'egg', num: 6 }		 }
+			 {type:'egg', num: 6 }		 ]
 	 });
 normalOrders.push({
 		 name:'Crybaby',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num:21},
 			 {type:'lettuce', num: 7 },
 			 {type:'meat', num: 14 }
-		}
+		]
 	 });
 normalOrders.push({
 		 name:'Salsaburger',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num:16},
 			 {type:'lettuce', num: 16 },
 			 {type:'meat', num: 16 }
-		}
+		]
 	 });
 	 
 normalOrders.push({
 		 name:'SkyBurger',
-		 ingredients:{
+		 ingredients:[
 			 {type:'onion', num:0},
 			 {type:'tomato', num:0 },
 			 {type:'cheese', num:0 },
 			 {type:'meat', num:0 },
 			 {type:'egg', num:0 },
-			 {type:'lettuce', num:0 },
+			 {type:'lettuce', num:0 }
 			 
-		 }
+		 ]
 });
 
 
@@ -136,7 +136,7 @@ Level.prototype.generateNextLevel = function() {
 	
 	if ( levelNum < 3 ) {
 		this.order = getOrder('easy');
-	} else if ( 0 == (levelNum % 10)  {
+	} else if ( 0 == (levelNum % 10))  {
 		this.order = getOrder('hard');
 	} else {
 		this.order = getOrder('normal');
