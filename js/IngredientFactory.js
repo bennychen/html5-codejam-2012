e62d49b84f8d53b1678e1b3b24d4d01c4fbcd32b
yaqi zhao <rainy_star_2001@hotmail.com>
2012-07-21 05:36:57 -0500

    fix bottom height

--- js/IngredientFactory.js
+++ js/IngredientFactory.js
@@ -21,7 +21,7 @@
 	IngredientFactory.prototype.CreatePlayer = function()
 	{
 		var height = utils.getIngredientHeight('bottom');
-		var player = new Ingredient("bottom",height);
+		var player = new Ingredient("bottom",100,height);
 		player.x = canvas.width/2;
 		player.y = canvas.height - 35;
 		player.catched = true;
