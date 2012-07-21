d004bebe982267870aa4fa5e0bd65c8f3eff3762
yaqi zhao <rainy_star_2001@hotmail.com>
2012-07-21 00:13:35 -0500

    first commit

---
+++ js/Ingredient.js
@@ -0,0 +1,39 @@
+(function(window) {
+	
+	function Ingredient(type) {
+		
+		this.oldX=0;
+		this.oldY=0;
+		this.imageSource = 'img/burger/' + type + '.png';
+		this.catched = false;
+		this.initialize();
+		
+		
+	}
+
+	Ingredient.prototype = new createjs.Container();
+
+	Ingredient.prototype.vx = 0;
+	Ingredient.prototype.vy = 10;
+	Ingredient.prototype.oldX;
+	Ingredient.prototype.oldY;
+	Ingredient.prototype.catched;
+	
+	Ingredient.prototype.imageSource;
+	
+	Ingredient.prototype.Container_initialize = Ingredient.prototype.initialize;
+    Ingredient.prototype.initialize = function() {
+        this.Container_initialize();
+        var bmp = new createjs.Bitmap(this.imageSource);
+        this.addChild(bmp);
+
+    }
+
+    function handleImageLoad() {
+       var bmp = new createjs.Bitmap(this.imageSource);
+       this.addChild(bmp);
+    }
+	
+	window.Ingredient = Ingredient;
+
+}(window));
