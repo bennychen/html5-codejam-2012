20b4e4246102f3ade3833984e985dd33b07e2268
Yu Hong <hello.hyu@gmail.com>
2012-07-22 13:13:16 +0800

    succes

--- js/inGameUI.js
+++ js/inGameUI.js
@@ -35,9 +35,14 @@ function setSuccessUI() {
 	for( i=0; i < level.order.ingredients.length; i++) {
 		var type = level.order.ingredients[i].type;
 		var num = level.order.ingredients[i].num;
-		alert(type);
 		summaryList.append('<li><span class="sammaryType">'+type+'</span> : <span class="sammaryNum">'+num+'</span></li>');
 	}
+	
+	$('#successTotal').text( '$' + orderSummary.subTotal );
+	
+	var tips = Math.round(orderSummary.subTotal * orderSummary.tips) / 100;
+	
+	$('#successTips').text( '$' + tips );
 }
 function showSuccessUI() {
 	orderSuccessUI.fadeIn();
